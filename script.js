54b6da43b46b9335d0f7974310fd5cfb58e11121
Anja <anjahauptman1@gmail.com>
2015-05-17 14:16:48 +0200

    -

--- script.js
+++ script.js
@@ -68,6 +68,7 @@ $(document).ready(function () {
     
     $(document).on("mousedown", "td.filmi", function() {
         var naslov = $(this).text();
+        alert(naslov);
         getDetails(naslov);
     });
     
