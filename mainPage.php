c2a6c73f063c8dc809a13cc9b144692b1402e0f0
Anja <anjahauptman1@gmail.com>
2015-05-18 20:09:02 +0200

    +

--- mainPage.php
+++ mainPage.php
@@ -94,11 +94,11 @@ session_start();
                         <input type="button" value="Gledano" id="watched_button"/>
                         <table id="ocena_filma">
                             <tr id="ocena_f">
-                            <td id="1star"></td>
-                            <td id="2star"></td>
-                            <td id="3star"></td>
-                            <td id="4star"></td>
-                            <td id="5star"></td>
+                            <td id="1star"><img src="star.png"></td>
+                            <td id="2star"><img src="star.png"></td>
+                            <td id="3star"><img src="star.png"></td>
+                            <td id="4star"><img src="star.png"></td>
+                            <td id="5star"><img src="star.png"></td>
                             </tr>
                         </table>
                         <h2 id="slo_naslov"></h2>
