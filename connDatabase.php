7d266e457b48f227e1d59a566ac1cdc56e1c047f
simona23 <simonasiljanovska23@gmail.com>
2015-05-25 10:47:54 +0200

    +

--- connDatabase.php
+++ connDatabase.php
@@ -32,4 +32,55 @@ if ($_POST['method'] == "getMovies")
         }
 }
 
+if ($_POST['method'] == "getFilm")
+{
+        $naslov = $_POST['movie_name'];
+        
+        $q = "SELECT * FROM Film WHERE slo_naslov = '$naslov'";
+        $result = mysqli_query($mysqli, $q);
+        
+		if (mysqli_num_rows($result) > 0) 
+		{
+        
+            $row = mysqli_fetch_assoc($result);
+            $i[0] = $row["slo_naslov"];
+            $i[1] = $row["ang_naslov"];
+            
+            $j = "Predlogi za film: <h2>$i[0]</h2>";
+            $j .= "<h3>$i[1]</h3>";
+            $prikaz[0] = $j;
+        } 
+		
+		else 
+		{
+            $prikaz[0] = "Film ni v bazi.";
+        }
+        
+        $q = "SELECT * FROM Film";
+        $result = mysqli_query($mysqli, $q);
+        
+        if (mysqli_num_rows($result) > 0) 
+		{    
+			$x = 0;
+            $j1 = "<tr>";
+            
+			while(($row = mysqli_fetch_assoc($result)) && $x < 5) 
+			{
+                $x++;
+                $j1 .= "<td class='f'>" . $row["slo_naslov"] . "</td>";
+            }
+            
+			$j1 .= "</tr>";
+            $prikaz[1] = $j1;
+        }
+        
+		else 
+		{
+            $prikaz[1] = "Ni priporocil.";
+        }
+        
+        $prikaz = json_encode($prikaz);
+        echo $prikaz;
+}
+
 ?>
