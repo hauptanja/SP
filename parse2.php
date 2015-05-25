61817140f28117a5953ebba07dc8e820a8f15fc5
Anja <anjahauptman1@gmail.com>
2015-05-25 18:03:25 +0200

    +

--- parse2.php
+++ parse2.php
@@ -19,7 +19,7 @@ echo "Connected successfully";
 
 foreach ($file->children() as $xml)
 {
-$q = "INSERT INTO TF-IDF (ID_filma, beseda, tf) VALUES ('$xml->id_filma', '$xml->beseda', '$xml->tf')";
+$q = "INSERT INTO 'TF-IDF' (ID_filma, beseda, tf) VALUES ('$xml->id_filma', '$xml->beseda', '$xml->tf')";
 
         if (mysqli_query($mysqli, $q)) {
             //echo "New record created successfully";
