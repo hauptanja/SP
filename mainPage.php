b6bea6a129575cf9800feb88601f5afdcdaf3a61
ursy <nemeturska@gmail.com>
2015-06-11 12:47:01 +0200

    .

--- mainPage.php
+++ mainPage.php
@@ -15,15 +15,6 @@ session_start();
 	    <div id ="website">
 		    <div id="banner"></div>
 		    <div id="fb-root"></div>
-	        <script>
-	            (function(d, s, id) {
-	              var js, fjs = d.getElementsByTagName(s)[0];
-	              if (d.getElementById(id)) return;
-	              js = d.createElement(s); js.id = id;
-	              js.src = "//connect.facebook.net/sl_SI/sdk.js#xfbml=1&version=v2.3";
-	              fjs.parentNode.insertBefore(js, fjs);
-	            }(document, 'script', 'facebook-jssdk'));
-	        </script>
 		    <ul class="tab-links">
 	            <?php 
 	                if(isset($_SESSION["username"])):?>
